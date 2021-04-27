e87cd847d5f8c450f2a80dddba3445285511737f
Connor <61102529+Metater@users.noreply.github.com>
2021-04-26 23:01:58 -0400

    Add PlayerInputPacket

---
+++ EfficaxData/Packets/Player/PlayerInputPacket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfficaxData.Packets.Player
+{
+    public class PlayerInputPacket : IEfficaxPacket
+    {
+        [Flags]
+        public enum PlayerInputs : byte
+        {
+            W0 = 1,
+            S0 = 2,
+            A0 = 4,
+            D0 = 8,
+            W1 = 16,
+            S1 = 32,
+            A1 = 64,
+            D1 = 128
+        }
+        public const int PacketId = 30;
+
+        public PlayerInputs playerInputs = 0;
+        public void FromPacket(DataReader dataReader)
+        {
+            throw new NotImplementedException();
+            playerInputs |= PlayerInputs.A0;
+        }
+
+        public byte[] ToPacket()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
